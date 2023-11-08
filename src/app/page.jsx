bf280213f82874e77509cfa927d765eec01ad1ba
GEdO23 <rm99632@fiap.com.br>
2023-11-08 09:13:41 -0300

    Novo icone e pagina home.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
       <h1>HOME</h1>
+
+      <div className="w-96 m-auto text-center">
+        <figure>
+          <Image src="/black-friday.png" alt="Black Friday" width={540} height={360}/>
+          <figcaption>Grande Promoção!!</figcaption>
+        </figure>
+      </div>
     </>
   );
 }
